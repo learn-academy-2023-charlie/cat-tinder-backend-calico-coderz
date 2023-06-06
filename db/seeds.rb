b410f9c5ad6d8705d49d8dca8fc3680bac3ee886
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-06 17:02:00 -0500

    dropped local db ST

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+cats = [
+    # {name:'Winston',
+    # age:3,
+    # hobbies:'hunting birds',
+    # image:"https://freesvg.org/img/OnlyWine-186.png"}
+]
+
 cats.each do |each_cat|
     Cat.create each_cat
     puts "creating cat #{each_cat}"
