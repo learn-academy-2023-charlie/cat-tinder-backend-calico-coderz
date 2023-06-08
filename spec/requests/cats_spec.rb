8c671679d44f4ba7415234ce89488e38cfa2e245
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-08 12:17:25 -0500

    complete api validations, model and requests

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -36,5 +36,18 @@ RSpec.describe "Cats", type: :request do
       
     end
   end
+  it "doesn't create a cat without a name" do
+    cat_params = {
+      cat: {
+        age: 3,
+        hobbies: 'hunts birds',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+      }
+    }
+    post '/cats', params: cat_params
+    expect(response.status).to eq 422
+    json = JSON.parse(response.body)
+    expect(json['name']).to include "can't be blank"
+  end
 end
 
